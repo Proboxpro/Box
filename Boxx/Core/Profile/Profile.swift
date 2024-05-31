2c0e7a8ef99eb507cc649c599e66c64f9c259f33
nikita evdokimov <evdokimovnikita1@gmail.com>
2024-05-31 23:27:54 +0300

    detailOrder:optimization:start

--- Boxx/Core/Profile/Profile.swift
+++ Boxx/Core/Profile/Profile.swift
@@ -174,18 +174,4 @@ struct Profile: View {
     }
 }
 
-extension UIImage {
-    func compressed(to quality: CGFloat) -> UIImage? {
-        guard let data = self.jpegData(compressionQuality: quality) else { return nil }
-        return UIImage(data: data)
-    }
-}
 
-extension UIImage {
-    func resize(to size: CGSize) -> UIImage? {
-        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
-        defer { UIGraphicsEndImageContext() }
-        self.draw(in: CGRect(origin: .zero, size: size))
-        return UIGraphicsGetImageFromCurrentImageContext()
-    }
-}
--- Boxx/Extensions/UIImage+MergeBySide.swift
+++ Boxx/Extensions/UIImage+MergeBySide.swift
@@ -9,6 +9,22 @@ import Foundation
 import UIKit
 
 extension UIImage {
+    
+    static func downloaded(from url: URL) async -> UIImage {
+        return await withCheckedContinuation { continuation in
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                guard
+                    let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                    let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                    let data1 = data, error == nil,
+                    let image = UIImage(data: data1)
+                else { return }
+                
+                continuation.resume(returning: image)
+            }.resume()
+        }
+    }
+    
     func mergedSideBySide(with otherImage: UIImage) -> UIImage? {
         let mergedWidth = self.size.width + otherImage.size.width
         let mergedHeight = max(self.size.height, otherImage.size.height)
@@ -19,5 +35,17 @@ extension UIImage {
         let mergedImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return mergedImage
+    }
+
+    func compressed(to quality: CGFloat) -> UIImage? {
+        guard let data = self.jpegData(compressionQuality: quality) else { return nil }
+        return UIImage(data: data)
+    }
+
+    func resize(to size: CGSize) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        defer { UIGraphicsEndImageContext() }
+        self.draw(in: CGRect(origin: .zero, size: size))
+        return UIGraphicsGetImageFromCurrentImageContext()
     }
 }
